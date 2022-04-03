ce7d4d07f5b2a98481d81ed6e8225efe61679036
ajay8115 <77052395+ajay8115@users.noreply.github.com>
2022-04-03 13:30:20 +0530

    Add files via upload

---
+++ longest_bitonic_subsequence.cpp
@@ -0,0 +1,47 @@
+// CODE BY :- AJAY PAL IIT (BHU) VARANASI
+#include <bits/stdc++.h>
+#define ll long long int
+#define ld long double
+#define kk '\n'
+using namespace std;
+
+class Solution
+{
+public:
+    int LongestBitonicSequence(vector<int> nums)
+    {
+        int n = nums.size();
+        int lis[n], lds[n];
+
+        lis[0] = 1;
+        lds[n - 1] = 1;
+
+        for (int i = 1; i < n; i++)
+        {
+            lis[i] = 1;
+            for (int j = 0; j < i; j++)
+            {
+                if (nums[j] < nums[i])
+                    lis[i] = max(lis[i], lis[j] + 1);
+            }
+        }
+
+        for (int i = n - 2; i >= 0; i--)
+        {
+            lds[i] = 1;
+            for (int j = n - 1; j > i; j--)
+            {
+                if (nums[j] < nums[i])
+                    lds[i] = max(lds[i], lds[j] + 1);
+            }
+        }
+
+        int ans = 0;
+        for (int i = 0; i < n; i++)
+        {
+            ans = max(ans, lis[i] + lds[i] - 1);
+        }
+
+        return ans;
+    }
+};
